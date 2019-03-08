7b20791e5f503f36afa4ae47c96a190ed932666c
a_morsi <adam.morsi@mindgeek.com>
2019-03-08 12:14:22 +0200

    Modify the custom validator file

--- src/customValidators/customValidator.js
+++ src/customValidators/customValidator.js
@@ -1,34 +1,3 @@
-export const checkValidity = (value, rules, inputIdentifier) => {
-    const clonedOrderForm   = {
-        ...this.state.orderForm
-    };
-    const clonedFormElement = {...clonedOrderForm[inputIdentifier]};
-
-    let isValid = true;
-    if (rules.required) {
-        isValid                                      = value.trim() !== '' && isValid;
-        clonedFormElement.displayErrors.showRequired = !(value.trim() !== '' && isValid);
-        clonedOrderForm[inputIdentifier]             = clonedFormElement;
-        this.setState({orderForm : clonedOrderForm});
-    }
-
-    if (rules.minLength) {
-        isValid                                       = value.length >= rules.minLength && isValid;
-        clonedFormElement.displayErrors.showMinLength = !(value.length >= rules.minLength && isValid);
-        clonedOrderForm[inputIdentifier]              = clonedFormElement;
-        this.setState({orderForm : clonedOrderForm});
-    }
-
-    if (rules.maxLength) {
-        isValid                                       = value.length <= rules.minLength && isValid;
-        clonedFormElement.displayErrors.showMaxLength = !(value.length <= rules.minLength && isValid);
-        clonedOrderForm[inputIdentifier]              = clonedFormElement;
-        this.setState({orderForm : clonedOrderForm});
-    }
-
-    return isValid;
-};
-
 // used to check if the given field is required
 export const validateRequired = (inputValue) => {
     return inputValue.trim() === '';
@@ -36,110 +5,110 @@ export const validateRequired = (inputValue) => {
 
 // used to validate the minLength of the given field
 export const validateMinLength = (inputValue, number) => {
-    let isValid = false;
+    let isError = false;
     if (inputValue.trim().length === 0) {
-        isValid = false;
+        isError = false;
     } else
         if (inputValue.trim().length < number) {
-            isValid = true;
+            isError = true;
         }
-    return isValid;
+    return isError;
 };
 
 // used to validate the maxLength of the given field
 export const validateMaxLength = (inputValue, number) => {
-    let isValid = false;
+    let isError = false;
     if (inputValue.trim().length === 0) {
-        isValid = false;
+        isError = false;
     } else
         if (inputValue.trim().length > number) {
-            isValid = true;
+            isError = true;
         }
-    return isValid;
+    return isError;
 };
 
 // used to validate the Google Analytics ID
 export const validateGoogleAnalytics = (inputValue) => {
-    let isValid = false;
+    let isError = false;
     if (inputValue.trim().length === 0) {
-        isValid = false;
+        isError = false;
     } else
         if (!(/^ua-\d{4,9}-\d{1,4}$/i.test(inputValue.trim()))) {
-            isValid = true;
+            isError = true;
         }
-    return isValid;
+    return isError;
 };
 
 // used to validate the given url
 export const validateUrl = (inputValue) => {
-    let isValid = false;
+    let isError = false;
     if (inputValue.trim().length === 0) {
-        isValid = false;
+        isError = false;
     } else
         if (!(/^(https?:\/\/)?(www.)[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/.test(inputValue.trim()))) {
-            isValid = true;
+            isError = true;
         }
-    return isValid;
+    return isError;
 };
 
 // used to validate instagram url
 export const validateInstagramUrl = (inputValue) => {
-    let isValid = false;
+    let isError = false;
     if (inputValue.trim().length === 0) {
-        isValid = false;
+        isError = false;
     } else
         if (!(/^(https?:\/\/)?(www.)instagram.com\/\w+(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/.test(inputValue))) {
-            isValid = true;
+            isError = true;
         }
-    return isValid;
+    return isError;
 };
 
 // used to validate facebook url
 export const validateFacebookUrl = (inputValue) => {
-    let isValid = false;
+    let isError = false;
     if (inputValue.trim().length === 0) {
-        isValid = false;
+        isError = false;
     } else
         if (!(/^(https?:\/\/)?(www.)facebook.com\/\w+(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/.test(inputValue.trim()))) {
-            isValid = true;
+            isError = true;
         }
-    return isValid;
+    return isError;
 };
 
 // used to validate snapchat url
 export const validateSnapchatUrl = (inputValue) => {
-    let isValid = false;
+    let isError = false;
     if (inputValue.trim().length === 0) {
-        isValid = false;
+        isError = false;
     } else
         if (!(/^(https?:\/\/)?(www.)snapchat.com\/\w+(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/.test(inputValue.trim()))) {
-            isValid = true;
+            isError = true;
         }
-    return isValid;
+    return isError;
 };
 
 // used to validate twitter url
 export const validateTwitterUrl = (inputValue) => {
-    let isValid = false;
+    let isError = false;
     if (inputValue.trim().length === 0) {
-        isValid = false;
+        isError = false;
     } else
         if (!(/^(https?:\/\/)?(www.)twitter.com\/\w+(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/.test(inputValue.trim()))) {
-            isValid = true;
+            isError = true;
         }
-    return isValid;
+    return isError;
 };
 
 // used to validate youtube url
 export const validateYoutubeUrl = (inputValue) => {
-    let isValid = false;
+    let isError = false;
     if (inputValue.trim().length === 0) {
-        isValid = false;
+        isError = false;
     } else
         if (!(/^(https?:\/\/)?(www.)youtube.com\/\w+(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/.test(inputValue.trim()))) {
-            isValid = true;
+            isError = true;
         }
-    return isValid;
+    return isError;
 };
 
 // used to validate the height and the width of file input type
